17c1b7649006e9eb851f709068e90bb15138b092
9-barristanselmy-9 <71235080+9-barristanselmy-9@users.noreply.github.com>
2025-05-09 12:34:18 +0100

    test husky

---
+++ app/dashboard/settings/_components/settings-header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface SettingsHeadProp {
+  title: string;
+}
+
+const SettingsHeader = ({ title }: SettingsHeadProp) => {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8 mb-6">
+      <h1 className="text-2xl font-normal">{title}</h1>
+    </div>
+  );
+};
+
+export default SettingsHeader;
